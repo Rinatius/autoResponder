0e66a4baba14b818cfe58892af6e5bbb52afe4ed
Rinat <rinat@kloop.kg>
2021-11-02 15:42:05 +0600

    Add ElasticSearch to API

--- responder/views.py
+++ responder/views.py
@@ -1,9 +1,15 @@
+import abc
+
 from django.contrib.admin.utils import unquote
 from django.shortcuts import render
+from elasticsearch_dsl import Q
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
 
+from responder.documents import QuestionDocument
 from responder.models import Campaign, Answer, Question
 from responder.serializer import CampaignSerializer, AnswerSerializer, QuestionSerializer
+from django.utils.translation import gettext_lazy as _
 
 
 # class FullTextQuestionSearchFilter(filters.BaseFilterBackend):
@@ -30,8 +36,88 @@ class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
 
 
+class ElasticSearchFilter(filters.SearchFilter):
+    serializer_class = None
+    document_class = None
+
+    search_param = "elastic"
+    template = 'rest_framework/filters/search.html'
+    search_title = _('Elastic Search')
+    search_description = _('A search term.')
+
+    @abc.abstractmethod
+    def generate_q_expression(self, query):
+        """This method should be overridden
+        and return a Q() expression."""
+
+    def get_search_terms(self, request):
+        """
+        Search term is set by a ?search=... query parameter.
+        """
+        params = request.query_params.get(self.search_param, '')
+        params = params.replace('\x00', '')  # strip null characters
+        params = params.replace(',', ' ')
+        return params
+
+    def filter_queryset(self, request, queryset, view):
+        print("Getting search results!!!")
+
+        search_fields = self.get_search_fields(view, request)
+        search_term = self.get_search_terms(request)
+
+        if not search_fields or not search_term:
+            return queryset
+
+        q = self.generate_q_expression(search_term)
+        search = self.document_class.search().query(q)
+        response = search.to_queryset()
+        print("Elastic search complete!!!")
+
+        queryset = queryset.intersection(response)
+        return queryset
+
+
+class QuestionElasticSearchFilter(ElasticSearchFilter):
+    serializer_class = QuestionSerializer
+    document_class = QuestionDocument
+
+    def generate_q_expression(self, query):
+        print("Generating Q expression!!!")
+        return Q(
+                'multi_match',
+                query=query,
+                fields=['text'],
+                fuzziness='auto')
+
+
 class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
-    filter_backends = [filters.SearchFilter]
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['text']
+    filter_backends = [filters.SearchFilter, QuestionElasticSearchFilter]
     search_fields = ['text']
+
+
+class ElasticSearchViewSet(viewsets.ViewSet):
+    serializer_class = None
+    document_class = None
+
+    @abc.abstractmethod
+    def generate_q_expression(self, query):
+        """This method should be overridden
+        and return a Q() expression."""
+
+    def get(self, request, query):
+        try:
+            q = self.generate_q_expression(query)
+            search = self.document_class.search().query(q)
+            response = search.execute()
+
+            print(f'Found {response.hits.total.value} hit(s) for query: "{query}"')
+
+            results = self.paginate_queryset(response, request, view=self)
+            serializer = self.serializer_class(results, many=True)
+            return self.get_paginated_response(serializer.data)
+        except Exception as e:
+            return HttpResponse(e, status=500)
