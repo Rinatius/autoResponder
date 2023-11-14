e431d25b414f11df3b2b17cdffe479bc29a3a005
karim <@1>
2023-11-14 15:13:00 +0300

    Fix ELASTICSEARCH_DSL const

--- autoResponder/settings.py
+++ autoResponder/settings.py
@@ -160,8 +160,11 @@ REST_FRAMEWORK = {
     ]
 }
 
+# Elasticsearch
+# https://django-elasticsearch-dsl.readthedocs.io/en/latest/settings.html
+
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'localhost:9200'
+        'hosts': 'http://localhost:9200'
     },
 }
