c5569c0828110a9d686625067cd198f92bd8dfed
karim <@1>
2023-11-22 15:37:54 +0300

    Update fixtures and Add bash script to add them

---
+++ responder/fixtures/loaddata.sh
@@ -0,0 +1,6 @@
+./../../manage.py loaddata campaigns.json
+./../../manage.py loaddata topics.json
+./../../manage.py loaddata languages.json
+
+./../../manage.py loaddata answers.json
+./../../manage.py loaddata questions.json
