a6bc472c0a2be596972752a7da706b6a0a210c7c
karim <@1>
2023-11-22 15:41:40 +0300

    Update ElasticSearch filters

--- responder/views.py
+++ responder/views.py
@@ -1,17 +1,19 @@
-import abc
-
-from django.contrib.admin.utils import unquote
-from django.shortcuts import render
 from elasticsearch_dsl import Q
-from rest_framework import viewsets, filters
-from rest_framework.response import Response
+from rest_framework import viewsets, filters, status
 
 from responder.apps import ResponderConfig
 from responder.documents import QuestionDocument, AnswerDocument
+from responder.exceptions import NotAQuestionException, NoAnswerToTheQuestionException
 from responder.models import Campaign, Answer, Question
-from responder.serializer import CampaignSerializer, AnswerSerializer, QuestionSerializer
+from responder.serializer import (
+    CampaignSerializer,
+    AnswerSerializer,
+    QuestionSerializer,
+)
 from django.utils.translation import gettext_lazy as _
 import tensorflow as tf
+
+from responder import utils
 
 
 # class FullTextQuestionSearchFilter(filters.BaseFilterBackend):
@@ -38,7 +40,7 @@ class ElasticSearchFilter(filters.SearchFilter):
     document_class = None
 
     search_param = "elastic"
-    search_title = _('Elastic Search')
+    search_title = _("Elastic Search")
 
     def generate_q_expression(self, query):
         """This method should be overridden
@@ -54,9 +56,9 @@ class ElasticSearchFilter(filters.SearchFilter):
         """
         Search term is set by a ?search=... query parameter.
         """
-        params = request.query_params.get(self.search_param, '')
-        params = params.replace('\x00', '')  # strip null characters
-        params = params.replace(',', ' ')
+        params = request.query_params.get(self.search_param, "")
+        params = params.replace("\x00", "")  # strip null characters
+        params = params.replace(",", " ")
         return params
 
     def filter_queryset(self, request, queryset, view):
@@ -76,10 +78,21 @@ class ElasticSearchFilter(filters.SearchFilter):
         print(results)
         for hit in results:
             print(hit.meta.score)
+
         response = search.to_queryset()
+        if self.document_class == AnswerDocument:
+            answer_options = [answer.english_text for answer in response]
+            best_answer = utils.generate_best_response(
+                utils.translate_text(search_term), answer_options
+            )
+            if best_answer != -1:
+                response = Answer.objects.filter(pk=response[best_answer].id)
+            else:
+                raise NoAnswerToTheQuestionException
+
         print("Elastic search complete!!!")
 
-        #queryset = queryset.intersection(response)
+        # queryset = queryset.intersection(response)
         return response
 
 
@@ -89,64 +102,52 @@ class QuestionElasticSearchFilter(ElasticSearchFilter):
 
     def generate_q_expression(self, query):
         print("Generating Q expression!!!")
-        return Q(
-            'multi_match',
-            query=query,
-            fields=['text'],
-            fuzziness='auto')
+        return Q("multi_match", query=query, fields=["text"], fuzziness="auto")
 
 
 class QuestionCosineElasticSearchFilter(ElasticSearchFilter):
     search_param = "cosine"
-    search_title = _('Cosine Elastic Search')
+    search_title = _("Cosine Elastic Search")
 
     serializer_class = QuestionSerializer
     document_class = QuestionDocument
 
     def generate_q_expression(self, query):
-        vector = ResponderConfig.neural_model.signatures['question_encoder'](
-            tf.constant([query, ]))
-        vector = list(vector['outputs'].numpy()[0])
-        q = Q("function_score",
-              # query=Q(
-              #     'bool',
-              #     must=[
-              #         Q('match', test='a'),
-              #     ]),
-              script_score={"script": {
-                  "source": "cosineSimilarity(params.queryVector, 'embedding') + 1.0",
-                  "params": {
-                      "queryVector": vector
-                  }
-              }},
-              )
+        query = utils.translate_text(query)
+
+        if not utils.is_question(query):
+            raise NotAQuestionException(_("Input is not a question"))
+
+        vector = ResponderConfig.neural_model.signatures["question_encoder"](
+            tf.constant(
+                [
+                    query,
+                ]
+            )
+        )
+        vector = list(vector["outputs"].numpy()[0])
+        q = Q(
+            "function_score",
+            # query=Q(
+            #     'bool',
+            #     must=[
+            #         Q('match', test='a'),
+            #     ]),
+            script_score={
+                "script": {
+                    "source": "cosineSimilarity(params.queryVector, 'embedding') + 1.0",
+                    "params": {"queryVector": vector},
+                }
+            },
+        )
         return q
 
-# class QuestionEuclideanElasticSearchFilter(ElasticSearchFilter):
-#     search_param = "euclidean"
-#     search_title = _('Euclidean Elastic Search')
-#
-#     serializer_class = QuestionSerializer
-#     document_class = QuestionDocument
-#
-#     def generate_q_expression(self, query):
-#         vector = ResponderConfig.neural_model.signatures['question_encoder'](
-#             tf.constant([query, ]))
-#         vector = list(vector['outputs'].numpy()[0])
-#         q = Q("function_score",
-#               # query=Q(
-#               #     'bool',
-#               #     must=[
-#               #         Q('match', test='a'),
-#               #     ]),
-#               script_score={"script": {
-#                   "source": "1 / (1 + l1norm(params.queryVector, 'embedding'))",
-#                   "params": {
-#                       "queryVector": vector
-#                   }
-#               }},
-#               )
-#         return q
+    def filter_queryset(self, request, queryset, view):
+        try:
+            return super().filter_queryset(request, queryset, view)
+        except NotAQuestionException:
+            raise NotAQuestionException
+
 
 class AnswerCosineElasticSearchFilter(QuestionCosineElasticSearchFilter):
     serializer_class = AnswerSerializer
@@ -158,35 +159,18 @@ class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     # filter_backends = [filters.SearchFilter]
     # search_fields = ['text']
-    filter_backends = [QuestionCosineElasticSearchFilter, filters.SearchFilter, QuestionElasticSearchFilter]
-    search_fields = ['text']
+    filter_backends = [
+        QuestionCosineElasticSearchFilter,
+        filters.SearchFilter,
+        QuestionElasticSearchFilter,
+    ]
+    search_fields = ["text"]
 
 
 class AnswerViewSet(viewsets.ModelViewSet):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
-    filter_backends = [AnswerCosineElasticSearchFilter, ]
-    search_fields = ['text']
-
-# class ElasticSearchViewSet(viewsets.ViewSet):
-#     serializer_class = None
-#     document_class = None
-#
-#     @abc.abstractmethod
-#     def generate_q_expression(self, query):
-#         """This method should be overridden
-#         and return a Q() expression."""
-#
-#     def get(self, request, query):
-#         try:
-#             q = self.generate_q_expression(query)
-#             search = self.document_class.search().query(q)
-#             response = search.execute()
-#
-#             print(f'Found {response.hits.total.value} hit(s) for query: "{query}"')
-#
-#             results = self.paginate_queryset(response, request, view=self)
-#             serializer = self.serializer_class(results, many=True)
-#             return self.get_paginated_response(serializer.data)
-#         except Exception as e:
-#             return HttpResponse(e, status=500)
+    filter_backends = [
+        AnswerCosineElasticSearchFilter,
+    ]
+    search_fields = ["text"]
