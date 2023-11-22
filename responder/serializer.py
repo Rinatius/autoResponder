2e2f94033fff926e3682f0d367937fe27e8a7866
karim <@1>
2023-11-22 15:36:40 +0300

    Update QuestionSerializer

--- responder/serializer.py
+++ responder/serializer.py
@@ -6,16 +6,18 @@ from responder.models import Campaign, Answer, Question
 class CampaignSerializer(serializers.ModelSerializer):
     class Meta:
         model = Campaign
-        fields = '__all__'
+        fields = "__all__"
 
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
-        fields = '__all__'
+        fields = "__all__"
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer()
+
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = ["id", "text", "campaign", "language", "answer", "topic"]
