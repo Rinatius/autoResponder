cfd27905571d7c20c809cd6b034587503792a9d4
karim <@1>
2023-11-22 15:36:14 +0300

    Add new Answer field "english_text", update save method in answer model, translate text before get embedding

--- responder/models.py
+++ responder/models.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+
 # import numpy as np
 import tensorflow_text
 from django.db import models
 
 from responder.apps import ResponderConfig
+from responder.utils import translate_text
 
 
 class Language(models.Model):
@@ -25,10 +27,7 @@ class Campaign(models.Model):
 class Topic(models.Model):
     name = models.CharField(max_length=400)
     campaign = models.ForeignKey(
-        Campaign,
-        on_delete=models.CASCADE,
-        related_name="topics",
-        help_text="Campaign"
+        Campaign, on_delete=models.CASCADE, related_name="topics", help_text="Campaign"
     )
 
     def __str__(self):
@@ -37,31 +36,47 @@ class Topic(models.Model):
 
 class Answer(models.Model):
     campaign = models.ForeignKey(
-        Campaign,
-        on_delete=models.CASCADE,
-        related_name="answers",
-        help_text="Campaign"
+        Campaign, on_delete=models.CASCADE, related_name="answers", help_text="Campaign"
     )
     language = models.ForeignKey(
         Language,
         on_delete=models.CASCADE,  # TODO Change deletion model
         related_name="answers",
-        help_text="Language"
+        help_text="Language",
     )
     text = models.TextField()
+    english_text = models.TextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.english_text:
+            self.english_text = translate_text(self.text)
+
+        super().save(*args, **kwargs)
 
     def get_embedding(self):
-        answer_context = self.text.split('. ', 1)
+        answer_context = self.english_text.split(". ", 1)
         answer = answer_context[0]
         if len(answer_context) > 1:
             context = answer_context[1]
         else:
             context = answer_context[0]
 
-        embeddings = ResponderConfig.neural_model.signatures['response_encoder'](
-            input=tf.constant([answer, ]),
-            context=tf.constant([context, ]))
-        return list(embeddings['outputs'].numpy()[0])
+        answer = translate_text(answer)
+        context = translate_text(context)
+
+        embeddings = ResponderConfig.neural_model.signatures["response_encoder"](
+            input=tf.constant(
+                [
+                    answer,
+                ]
+            ),
+            context=tf.constant(
+                [
+                    context,
+                ]
+            ),
+        )
+        return list(embeddings["outputs"].numpy()[0])
 
     def __str__(self):
         return self.text
@@ -72,36 +87,39 @@ class Question(models.Model):
         Campaign,
         on_delete=models.CASCADE,
         related_name="questions",
-        help_text="Campaign"
+        help_text="Campaign",
     )
     language = models.ForeignKey(
         Language,
         on_delete=models.CASCADE,  # TODO Change deletion model
         related_name="questions",
-        help_text="Language"
+        help_text="Language",
     )
     answer = models.ForeignKey(
-        Answer,
-        on_delete=models.CASCADE,
-        related_name="questions",
-        help_text="Answer"
+        Answer, on_delete=models.CASCADE, related_name="questions", help_text="Answer"
     )
     topic = models.ForeignKey(
         Topic,
         on_delete=models.CASCADE,  # TODO Change deletion model
         related_name="questions",
-        help_text="Topic"
+        help_text="Topic",
     )
     text = models.TextField()
+
     # search_vector = SearchVectorField(null=True, blank=True)
     #
     # class Meta:
     #     indexes = (GinIndex(fields=["search_vector"]),)
 
     def get_embedding(self):
-        embeddings = ResponderConfig.neural_model.signatures['question_encoder'](
-            tf.constant([self.text, ]))
-        return list(embeddings['outputs'].numpy()[0])
+        embeddings = ResponderConfig.neural_model.signatures["question_encoder"](
+            tf.constant(
+                [
+                    self.text,
+                ]
+            )
+        )
+        return list(embeddings["outputs"].numpy()[0])
 
     def __str__(self):
         return self.text
